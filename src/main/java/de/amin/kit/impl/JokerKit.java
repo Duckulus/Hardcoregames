4f6ea5fe1dd56764104ef17103a6a843caa1d30d
Duckulus <76813487+Duckulus@users.noreply.github.com>
2021-09-12 15:02:56 +0200

    a

--- src/main/java/de/amin/kit/impl/JokerKit.java
+++ src/main/java/de/amin/kit/impl/JokerKit.java
@@ -92,11 +92,9 @@ public class JokerKit extends Kit implements Listener {
     }
 
     private void shuffle(ItemStack[] ar) {
-        // If running on Java 6 or older, use `new Random()` on RHS here
         Random rnd = ThreadLocalRandom.current();
         for (int i = ar.length - 1; i > 0; i--) {
             int index = rnd.nextInt(i + 1);
-            // Simple swap
             ItemStack a = ar[index];
             ar[index] = ar[i];
             ar[i] = a;
